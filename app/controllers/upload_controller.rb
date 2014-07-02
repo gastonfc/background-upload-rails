c0406507928c6a78a5083af3208b0d0e5782f190
Gastón Fernández <gaston.fernandez@pyxis.com.uy>
2014-07-02 10:50:22 -0300

    Upload básico funcionando

---
+++ app/controllers/upload_controller.rb
@@ -0,0 +1,11 @@
+class UploadController < ApplicationController
+  def index
+     render :file => '/app/views/upload/uploadfile.rhtml'
+  end
+
+  def uploadFile
+    params.inspect
+    file = DataFile.save(params[:upload])
+    render :text => "File has been uploaded successfully"
+  end
+end
